6004aebd8ab3c988199c4afe6782f441143d017d
Ivan Kleshnin <ivan@paqmind.com>
2022-06-15 17:55:25 +0200

    Stuff

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,61 +1,61 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import { marked } from 'marked'
-import Link from 'next/link'
-import { slugify, ImageUrl } from '../../utils'
-import { NextSeo } from 'next-seo';
+import fs from "fs"
+import path from "path"
+import parseFM from "gray-matter"
+import { marked } from "marked"
+import Link from "next/link"
+import {slugify, ImageUrl} from "../../utils"
+import {NextSeo} from "next-seo"
 
-export default function PostPage({ content, frontmatter }) {
-  const date = new Date(frontmatter.date)
-  const imageMeta= frontmatter.images.map(image => {
-     const imageUrl =  ImageUrl(image)
+export default function PostPage({post}) {
+  const date = new Date(post.date)
+  const imageMeta = post.images.map(image => {
+     const imageUrl = ImageUrl(image)
       return {
       url: imageUrl,
       width: 1224,
       height: 724,
-      alt: frontmatter.title,
-      type: 'image/jpeg',
+      alt: post.title,
+      type: "image/jpeg",
     }
   })
 
   return <>
     <NextSeo
-      title={frontmatter.title}
-      description={frontmatter.summary}
+      title={post.title}
+      description={post.summary}
       openGraph={{
-        url: 'https:officialrajdeepsingh.dev',
-        title: frontmatter.title,
-        description: frontmatter.summary ,
-        type: 'article',
+        url: "https:officialrajdeepsingh.dev",
+        title: post.title,
+        description: post.summary ,
+        type: "article",
         article: {
-          publishedTime: frontmatter.date,
+          publishedTime: post.date,
           authors: [
-            'https://officialrajdeepsingh.dev/pages/about',
+            "https://officialrajdeepsingh.dev/pages/about",
           ],
-          tags: frontmatter.tags,
+          tags: post.tags,
         },
         images: imageMeta,
-        site_name: 'Rajdeep Singh',
+        site_name: "Rajdeep Singh",
       }}
     />
     <div className="container my-5">
       <div className="row">
         <div className="col-lg-10 m-auto">
-          <div className='card card-page'>
-            <a href={`/blog/${frontmatter.slug}`} > <img className="card-img-top" src={ImageUrl(frontmatter.image)} alt="..." /></a>
+          <div className="card card-page">
+            <a href={`/blog/${post.slug}`} > <img className="card-img-top" src={ImageUrl(post.image)} alt="..." /></a>
 
-            <h1 className='post-title mt-2 p-2'>{frontmatter.title}</h1>
-            <div className='post-date m-1 p-2'>
+            <h1 className="post-title mt-2 p-2">{post.title}</h1>
+            <div className="post-date m-1 p-2">
               <div>
                 <h6>{`${date.getMonth() + 1} - ${date.getDate()} - ${date.getFullYear()}`} </h6>
               </div>
               <div>
-                {frontmatter.tags.map(tag => {
+                {post.tags.map(tag => {
                   const slug = slugify(tag)
                   return <Link key={tag} href={`/tag/${slug}`}>
-                    <a className='btn'>
-                      <h6 className='post-title'>#{tag}</h6>
+                    <a className="btn">
+                      <h6 className="post-title">#{tag}</h6>
                     </a>
                   </Link>
                 })}
@@ -63,8 +63,8 @@ export default function PostPage({ content, frontmatter }) {
             </div>
 
             <div
-              className='post-body p-5 m-auto'
-             dangerouslySetInnerHTML={{__html: marked.parse(content)}}
+              className="post-body p-5 m-auto"
+              dangerouslySetInnerHTML={{__html: marked.parse(post.content)}}
             />
           </div>
         </div>
@@ -73,55 +73,72 @@ export default function PostPage({ content, frontmatter }) {
   </>
 }
 
+// TODO apply marked on BE!
 
 export async function getStaticPaths() {
-  // console.log("process.cwd():", fs.readdirSync(process.cwd()))
-  // console.log("./content", fs.readdirSync(path.resolve("./content")))
-  //
-  // //  Get files from the posts dir
-  // const files = fs.readdirSync(path.resolve("content", "blog"))
-  //
-  //  // Get slug and frontmatter from posts
-  // const temppaths = files.map((filename) => {
-  //
-  //   // Get frontmatter
-  //   const markdownWithMeta = fs.readFileSync(
-  //     path.join("content", "blog", filename),
-  //     'utf-8'
-  //   )
-  //
-  //   const { data: frontmatter } = matter(markdownWithMeta)
-  //
-  //   return {
-  //     params: {
-  //       slug: filename.replace('.md', ''),
-  //     },
-  //   }
-  // })
-  // //   remove null in tempPosts
-  // const paths = temppaths.filter(
-  //   path => {
-  //     return path && path
-  //   }
-  // )
-
   return {
     paths: [],
-    fallback: "blocking", // false,
+    fallback: "blocking",
   }
 }
 
+export async function getStaticProps({params}) {
+  const {slug} = params
 
-export async function getStaticProps({ params: { slug } }) {
-  const markdownWithMeta = fs.readFileSync(path.join("content", "blog", slug + ".md"), 'utf-8')
-  console.log("markdownWithMeta:", markdownWithMeta)
-  const {data: frontmatter, content} = matter(markdownWithMeta)
+  console.log("process.cwd():", fs.readdirSync(process.cwd()))
+  console.log("./", fs.readdirSync(path.resolve("./")))
+
+  const post = readFM(path.resolve("content", "blog", slug + ".md"))
 
   return {
     props: {
-      frontmatter,
-      slug,
-      content,
+      post,
     },
   }
 }
+
+function readFM(file) {
+  const fm = fs.readFileSync(file, "utf-8")
+  const slug = path.basename(file).replace(/.md$/, "")
+  const {data, content} = parseFM(fm)
+  return {
+    ...data, content, slug
+  }
+}
+
+// export async function getStaticPaths() {
+//   // console.log("process.cwd():", fs.readdirSync(process.cwd()))
+//   // console.log("./content", fs.readdirSync(path.resolve("./content")))
+//   //
+//   // //  Get files from the posts dir
+//   // const files = fs.readdirSync(path.resolve("content", "blog"))
+//   //
+//   //  // Get slug and frontmatter from posts
+//   // const temppaths = files.map((filename) => {
+//   //
+//   //   // Get frontmatter
+//   //   const markdownWithMeta = fs.readFileSync(
+//   //     path.join("content", "blog", filename),
+//   //     "utf-8"
+//   //   )
+//   //
+//   //   const {data} = matter(markdownWithMeta)
+//   //
+//   //   return {
+//   //     params: {
+//   //       slug: filename.replace(".md", ""),
+//   //     },
+//   //   }
+//   // })
+//   // //   remove null in tempPosts
+//   // const paths = temppaths.filter(
+//   //   path => {
+//   //     return path && path
+//   //   }
+//   // )
+//
+//   return {
+//     paths: [],
+//     fallback: "blocking", // false,
+//   }
+// }
