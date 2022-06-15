6a4d20ccef041921c729d68d6ccba8abb035b589
Ivan Kleshnin <ivan@paqmind.com>
2022-06-15 18:26:39 +0200

    Stuff

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+// import fs from 'fs'
+// import path from 'path'
 import matter from 'gray-matter'
 import { NextSeo } from 'next-seo';
 import Post from '../components/Post'
